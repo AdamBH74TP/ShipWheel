8e11dbcc92c14eed45448c2bac0ba10b6f2e7982
AdamBH74TP <bh74tp@student.sunderland.ac.uk>
2022-09-28 13:07:21 +0100

    Initial commit

---
+++ Assets/Scripts/WheelSpin.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody wheelRB;
+    private void Start()
+    {
+        if (GameObject.Find("Wheel").GetComponent<Rigidbody>() == null)
+        {
+            GameObject.Find("Wheel").AddComponent<Rigidbody>();
+        }
+        wheelRB = GameObject.Find("Wheel").GetComponent<Rigidbody>();
+        wheelRB.useGravity = false;
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            wheelRB.MoveRotation(wheelRB.rotation * Quaternion.Euler(0, 0, 1));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            wheelRB.MoveRotation(wheelRB.rotation * Quaternion.Euler(0, 0, -1));
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            wheelRB.rotation = Quaternion.identity;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ShipWheel
+ 
